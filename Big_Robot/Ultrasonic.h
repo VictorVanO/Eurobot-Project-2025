2e34ce84c67ddfde96d09b53902e28d1a02c98f0
VictorVanO <20253@ecam.be>
2025-02-21 15:47:03 +0100

    Changed ultrasonic sensors pins

--- Big_Robot/Ultrasonic.h
+++ Big_Robot/Ultrasonic.h
@@ -5,8 +5,8 @@
 #define NUM_ULTRASONIC 3  
 
 // Define trig and echo pins for each sensor
-const int trigPins[NUM_ULTRASONIC] = {9, 7, 5};
-const int echoPins[NUM_ULTRASONIC] = {10, 8, 6};
+const int trigPins[NUM_ULTRASONIC] = {22, 26, 30};
+const int echoPins[NUM_ULTRASONIC] = {24, 28, 32};
 
 void initUltrasonic();
 float readDistance(int sensorIndex);
