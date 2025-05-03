91da4e45ea6a36350fd88a2cebd322804c045e62
AngeEcam <124886512+AngeEcam@users.noreply.github.com>
2025-05-04 01:36:43 +0200

    Delete unnecessary comments

--- groupie-robot/Tirette.h
+++ groupie-robot/Tirette.h
@@ -5,10 +5,9 @@
 
 class Tirette {
   public:
-    Tirette(uint8_t pin);     // Constructeur avec la pin à laquelle la tirette est connectée
-    void begin();             // Initialise la pin en mode INPUT_PULLUP
-    bool estActivee();        // Retourne true si la tirette est tirée (contact fermé)
-
+    Tirette(uint8_t pin);     
+    void begin();             
+    bool estActivee();        
   private:
     uint8_t _pin;
 };
