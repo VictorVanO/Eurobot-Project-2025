f78c021f4dc3f64d8b7bb24e39dff6cdadb90979
AngeEcam <124886512+AngeEcam@users.noreply.github.com>
2025-05-04 01:35:09 +0200

    Delete unnecessary comments

--- groupie-robot/Infrared.cpp
+++ groupie-robot/Infrared.cpp
@@ -8,26 +8,26 @@ void initInfrared() {
 
 
 int returnDirection() {
-  int leftIR = digitalRead(LEFT_IR_SENSOR);   // 1 = noir
-  int rightIR = digitalRead(RIGHT_IR_SENSOR); // 1 = noir
+  int leftIR = digitalRead(LEFT_IR_SENSOR);   // 1 = black
+  int rightIR = digitalRead(RIGHT_IR_SENSOR); // 1 = black
 
   // Cas 1 : 
   if (leftIR == HIGH && rightIR == HIGH) {
-    return 0; // Avancer
+    return 0; // Go forward
   }
 
   // Cas 2 :
   else if (leftIR == HIGH && rightIR == LOW) {
-    return 1; // Tourner à droite
+    return 1; // Turn right
   }
 
   // Cas 3 : 
   else if (leftIR == LOW && rightIR == HIGH) {
-    return 2; // Tourner à gauche
+    return 2; // Turn right
   }
 
   // Cas 4 : 
   else if (leftIR == LOW && rightIR == LOW) {
-    return 3; // 
+    return 3;  
   }
 }
